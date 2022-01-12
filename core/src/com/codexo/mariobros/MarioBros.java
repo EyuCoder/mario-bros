819df176f3d92287fd7dce32e067974cfd23ab53
elysium09 <eyueldaniel0921@gamil.com>
2022-01-12 18:18:15 +0300

    :tada: INIT

---
+++ core/src/com/codexo/mariobros/MarioBros.java
@@ -0,0 +1,20 @@
+package com.codexo.mariobros;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.codexo.mariobros.screens.PlayScreen;
+
+public class MarioBros extends Game {
+	public SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+}
