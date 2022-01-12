566dcfab352e5bb2b5175592193cf4646e8ec2e1
elysium09 <eyueldaniel0921@gamil.com>
2022-01-12 19:13:30 +0300

    :sparkles: HUD

---
+++ core/src/com/codexo/mariobros/scenes/Hud.java
@@ -0,0 +1,54 @@
+package com.codexo.mariobros.scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.codexo.mariobros.MarioBros;
+
+import java.util.Locale;
+
+public class Hud {
+    public Stage stage;
+    private Viewport viewport;
+
+    private Integer worldTimer, score;
+    private Float timeCount;
+
+    Label countDownLabel, scoreLabel, timeLabel, levelLabel, worldLabel, marioLabel;
+
+    public Hud(SpriteBatch spriteBatch) {
+        worldTimer = 300;
+        timeCount = 0F;
+        score = 0;
+
+        viewport = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, spriteBatch);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        countDownLabel = new Label(String.format(Locale.US, "%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel = new Label(String.format(Locale.US, "%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        table.add(marioLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(levelLabel).expandX();
+        table.add(countDownLabel).expandX();
+
+        stage.addActor(table);
+    }
+}
--- core/src/com/codexo/mariobros/screens/PlayScreen.java
+++ core/src/com/codexo/mariobros/screens/PlayScreen.java
@@ -3,15 +3,24 @@ package com.codexo.mariobros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.codexo.mariobros.MarioBros;
+import com.codexo.mariobros.scenes.Hud;
 
 public class PlayScreen implements Screen {
     private MarioBros game;
-    Texture texture;
+    private OrthographicCamera cam;
+    private Viewport viewport;
+    private Hud hud;
+
     public PlayScreen(MarioBros game) {
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+        cam = new OrthographicCamera();
+        viewport = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, cam);
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -21,11 +30,10 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture, 0,0);
-        game.batch.end();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
